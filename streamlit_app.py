1a1d2098d2377df1505608b86cceb1b321f6af4e
Ignition_Group <108255732+IGNITION-DISTRIBUTION-TEAM@users.noreply.github.com>
2023-01-10 00:15:26 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -17,6 +17,7 @@ mainschema = "DISTRIBUTION_DATA_APPLICATION"
 def  connect_to_snowflake(acc,user,passw,sch,wh,db):
     ctx = sf.connect(account=acc,username=user,password=passw,schema=sch,warehouse=wh,database=db)
     cs = ctx.cursor()
+    st.session_state['is_ready'] = True
     return cs
     
 
@@ -24,3 +25,11 @@ with sidebar:
     Username = st.text_input("Username")
     password = st.text_input("Password")
     connect = st.button("Connect to Snowflake",on_click=connect_to_snowflake, args=[account,Username,password,schema,warehouse,database])
+    
+
+
+if 'is_ready' not in st.session_state:
+    st.session_state [is_ready] = False
+
+if st.session_state['is_ready'] == True:
+    st.write("Connected")
