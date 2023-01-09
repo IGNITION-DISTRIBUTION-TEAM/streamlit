0467f30642c7b76093937e1c593828e0b6e3d64b
Ignition_Group <108255732+IGNITION-DISTRIBUTION-TEAM@users.noreply.github.com>
2023-01-09 17:06:19 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,34 +1,27 @@
-import pandas as pd
-import numpy as np
+
 import streamlit as st
-import pandas as pd
-from snowflake.snowpark import Session
+import snowflake.connector
 
-mainaccount = "pm58521.east-us-2.azure"
-mainuser = "SVC_DISTRIBUTION_DATA"
-mainpassword = "khrKV3ymWLvMg6QczKMr!!"
-mainwarehouse = "DISTRIBUTION_WH"
-maindatabase = "DATAWAREHOUSE"
-mainschema = "DISTRIBUTION_DATA_APPLICATION"
-SProcedure = "SP_DMASA_AUTOMATION"
+# Initialize connection.
+# Uses st.experimental_singleton to only run once.
+@st.experimental_singleton
+def init_connection():
+    return snowflake.connector.connect(
+        **st.secrets["snowflake"], client_session_keep_alive=True
+    )
 
-connection_parameters = { "account": mainaccount,
-                         "user": mainuser,
-                         "password": mainpassword,
-                         "warehouse": mainwarehouse,
-                         "database": maindatabase,
-                         "schema": mainschema} 
+conn = init_connection()
 
+# Perform query.
+# Uses st.experimental_memo to only rerun when the query changes or after 10 min.
+@st.experimental_memo(ttl=600)
+def run_query(query):
+    with conn.cursor() as cur:
+        cur.execute(query)
+        return cur.fetchall()
 
-new_session = Session.builder.configs(connection_parameters).create()  
+rows = run_query("SELECT * from mytable;")
 
-procedureruninfo = new_session.call(SProcedure) 
-
-viewdata = Session.table(new_session,"VW_SALES_ALL_CAMPAIGNS").collect()
-
-snowflake_df = Session.create_dataframe(new_session,viewdata)
-pandas_df = snowflake_df.to_pandas()
-
-new_session.close()
-
-st.line_chart(pandas_df)
+# Print results.
+for row in rows:
+    st.write(f"{row[0]} has a :{row[1]}:")
