2a812a83f5b1790c71d17eaf1a95a6521755925b
Ignition_Group <108255732+IGNITION-DISTRIBUTION-TEAM@users.noreply.github.com>
2023-01-10 00:44:19 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -22,7 +22,7 @@ def  connect_to_snowflake(acc,user,passw,sch,wh,db):
     return cs
 
 def get_data():
-    query = 'select * from VW_SALES_ALL_CAMPAIGNS'
+    query = 'select * from DATAWAREHOUSE.DISTRIBUTION_DATA_APPLICATION.VW_SALES_ALL_CAMPAIGNS'
     results = st.session_state['Snow_conn'].execute(query)
     results = st.session_state['Snow_conn'].fetch_pandas_all()
     return results
