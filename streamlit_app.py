1c981620fdea4598ce7c6103f5a3620294674e64
Ignition_Group <108255732+IGNITION-DISTRIBUTION-TEAM@users.noreply.github.com>
2023-01-10 00:11:17 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,9 +4,23 @@ import pandas as pd
 import snowflake.connector as sf
 from datetime import date
 
-
 sidebar = st.sidebar
 
+account = "pm58521.east-us-2.azure"
+role = "role"
+warehouse = "DISTRIBUTION_WH"
+database = "DATAWAREHOUSE"
+schema = "DISTRIBUTION_DATA_APPLICATION"
+
+mainschema = "DISTRIBUTION_DATA_APPLICATION"
+
+def  connect_to_snowflake(acc,user,passw,sch,wh,db):
+    ctx = sf.connect(account=acc,username=user,password=passw,schema=sch,warehouse=wh,database=db)
+    cs = ctx.cursor()
+    return cs
+    
+
 with sidebar:
-    account = st.text_input("Username")
+    Username = st.text_input("Username")
     password = st.text_input("Password")
+    connect = st.button("Connect to Snowflake",on_click=connect_to_snowflake, args=[account,Username,password,schema,warehouse,database])
