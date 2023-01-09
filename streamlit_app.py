cb94aee09426c62605c3d129ab9846a5f19a33fe
Ignition_Group <108255732+IGNITION-DISTRIBUTION-TEAM@users.noreply.github.com>
2023-01-10 00:30:35 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,21 +14,21 @@ sidebar = st.sidebar
 mainschema = "DISTRIBUTION_DATA_APPLICATION"
 
 def  connect_to_snowflake(acc,user,passw,sch,wh,db):
-    ctx = sf.connect(account=acc,username=user,password=passw,schema=sch,warehouse=wh,database=db)
+    ctx = sf.connect(account=acc,user=user,password=passw,schema=sch,warehouse=wh,database=db)
     cs = ctx.cursor()
     st.session_state['is_ready'] = True
     return cs
     
 
 with sidebar:
-    account = "pm58521.east-us-2.azure"
-    User = st.text_input("Username")
+    Username = st.text_input("Username")
     password = st.text_input("Password")
+    account = "pm58521.east-us-2.azure"
     role = "role"
     warehouse = "DISTRIBUTION_WH"
     database = "DATAWAREHOUSE"
     schema = "DISTRIBUTION_DATA_APPLICATION"
-    connect = st.button("Connect to Snowflake",on_click=connect_to_snowflake, args=[account,User,password,schema,warehouse,database])
+    connect = st.button("Connect to Snowflake",on_click=connect_to_snowflake, args=[account,Username,password,schema,warehouse,database])
     
 
 
@@ -37,4 +37,3 @@ if 'is_ready' not in st.session_state:
 
 if st.session_state['is_ready'] == True:
     st.write("Connected")
-    
