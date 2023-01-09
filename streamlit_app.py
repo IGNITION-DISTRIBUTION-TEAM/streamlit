e15adfe805721366a0f620c4b6b2bda4297cd6b1
Ignition_Group <108255732+IGNITION-DISTRIBUTION-TEAM@users.noreply.github.com>
2023-01-09 17:09:00 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -20,7 +20,7 @@ def run_query(query):
         cur.execute(query)
         return cur.fetchall()
 
-rows = run_query("SELECT * from mytable;")
+rows = run_query("SELECT * from VW_SALES_ALL_CAMPAIGNS;")
 
 # Print results.
 for row in rows:
