66c1a52011ddd0287023ecd1e2162f2b788e15f1
Ignition_Group <108255732+IGNITION-DISTRIBUTION-TEAM@users.noreply.github.com>
2023-01-10 22:13:03 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,47 +1,48 @@
-import toml
+from snowflake.snowpark.session import Session
+from snowflake.snowpark.functions import avg, sum, col,lit
 import streamlit as st
 import pandas as pd
-import snowflake.connector as sf
-from datetime import date
 
-sidebar = st.sidebar
+st.set_page_config(
+     page_title="Environment Data Atlas",
+     page_icon="🧊",
+     layout="wide",
+     initial_sidebar_state="expanded",
+     menu_items={
+         'Get Help': 'https://developers.snowflake.com',
+         'About': "This is an *extremely* cool app powered by Snowpark for Python, Streamlit, and Snowflake Data Marketplace"
+     }
+)
 
+# Create Session object
+def create_session_object():
+    connection_parameters = {
+      "account": "pm58521.east-us-2.azure",
+      "user": "SVC_DISTRIBUTION_DATA",
+      "password": "<khrKV3ymWLvMg6QczKMr!!",
+      "warehouse": "DISTRIBUTION_WH",
+      "database": "DATAWAREHOUSE",
+      "schema": "DISTRIBUTION_DATA_APPLICATION"
+    }
+    session = Session.builder.configs(connection_parameters).create()
+    print(session.sql('select current_warehouse(), current_database(), current_schema()').collect())
+    return session
 
-
-#mainuser = "SVC_DISTRIBUTION_DATA"
-#mainpassword = "khrKV3ymWLvMg6QczKMr!!"
-
-mainschema = "DISTRIBUTION_DATA_APPLICATION"
-
-def  connect_to_snowflake(acc,user,passw,sch,wh,db):
-    ctx = sf.connect(account=acc,user=user,password=passw,schema=sch,warehouse=wh,database=db)
-    cs = ctx.cursor()
-    # keep connection in memery
-    st.session_state['Snow_conn'] = cs
-    st.session_state['is_ready'] = True
-    return cs
-
-def get_data():
-    query = 'select 1'
-    return = st.session_state['Snow_conn'].execute(query).fetch_pandas_all()
-    return results
-
-with sidebar:
-    Username = st.text_input("Username")
-    password = st.text_input("Password")
-    account = "pm58521.east-us-2.azure"
-    role = "role"
-    warehouse = "DISTRIBUTION_WH"
-    database = "DATAWAREHOUSE"
-    schema = "DISTRIBUTION_DATA_APPLICATION"
-    connect = st.button("Connect to Snowflake",on_click=connect_to_snowflake, args=[account,Username,password,schema,warehouse,database])
+# Add header and a subheader
+st.header("Knoema: Environment Data Atlas")
+st.subheader("Powered by Snowpark for Python and Snowflake Data Marketplace | Made with Streamlit")
+  
+# Create Snowpark DataFrames that loads data from Knoema: Environmental Data Atlas
+def load_data(session):
+    # CO2 Emissions by Country
+    snow_df_co2 = session.table("DISTRIBUTION_DATA_APPLICATION.TW_SAC_SALES_ALL_CAMPAIGNS")
     
-
-
-if 'is_ready' not in st.session_state:
-    st.session_state ['is_ready'] = False
-
-if st.session_state['is_ready'] == True:
-    st.write("Connected")
-    data = get_data()
-    st.dataframe(data)
+    # Convert Snowpark DataFrames to Pandas DataFrames for Streamlit
+    pd_df_co2  = snow_df_co2.to_pandas()
+    
+    with st.container():
+            st.dataframe(pd_df_co2)
+    
+if __name__ == "__main__":
+    session = create_session_object()
+    load_data(session)
