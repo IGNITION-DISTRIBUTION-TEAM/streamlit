a24b221d5fd76baed73f0a1c105a308758d7b922
Ignition_Group <108255732+IGNITION-DISTRIBUTION-TEAM@users.noreply.github.com>
2023-03-14 23:36:10 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,8 +6,7 @@ import snowflake.connector
 # Initialize connection.
 # Uses st.cache_resource to only run once.
 
-with st.sidebar:
-    my_component(greeting="Peekabo")
+
 
 def init_connection():
     return snowflake.connector.connect(
@@ -43,3 +42,6 @@ chart_data = pd.DataFrame(
     columns=['a', 'b', 'c'])
 
 st.line_chart(chart_data)
+
+with st.sidebar:
+    my_component(greeting="Peekabo")
