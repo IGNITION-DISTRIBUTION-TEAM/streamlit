8d76f0645bacd6dfd961132354d39135426e3e90
Ignition_Group <108255732+IGNITION-DISTRIBUTION-TEAM@users.noreply.github.com>
2023-03-16 14:43:30 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,10 +7,8 @@ import snowflake.connector
 # Uses st.cache_resource to only run once.
 
 
-st.write('Hello, *World!* :sunglasses:')
+st.title('Onair Campaign Stats')
 
-
-st.button('new')
 
 
 def init_connection():
@@ -29,28 +27,3 @@ def run_query(query):
     with conn.cursor() as cur:
         cur.execute(query)
         return cur.fetchall()
-
-rows = run_query("SELECT * from TM_API_SCHEDULETYPES;")
-
-
-chart_data = pd.DataFrame(
-    np.random.randn(20, 3),
-    columns=['a', 'b', 'c'])
-
-st.line_chart(chart_data)
-
-    
-col1, col2, col3 = st.columns(3)
-
-with col1:
-    if st.button('show me your table!'):
-        st.dataframe(rows)
-    else:
-        st.write('I wont do it')
-
-with col2:
-    st.metric(label="Temperature", value="70 °F", delta="1.2 °F")
-
-with col3:
-   st.header("An owl")
-   st.image("https://static.streamlit.io/examples/owl.jpg")    
