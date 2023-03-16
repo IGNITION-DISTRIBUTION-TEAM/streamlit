457c25db919aa18905cae0fff118c904502a0bab
Ignition_Group <108255732+IGNITION-DISTRIBUTION-TEAM@users.noreply.github.com>
2023-03-16 22:13:52 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -31,7 +31,19 @@ st.dataframe(rslt_df)
 st.line_chart(rslt_df,x="date",y="totalagents")
 
 
+col1, col2, col3 = st.columns(3)
 
+with col1:
+   st.header("A cat")
+   st.image("https://static.streamlit.io/examples/cat.jpg")
+
+with col2:
+   st.header("A dog")
+   st.image("https://static.streamlit.io/examples/dog.jpg")
+
+with col3:
+   st.header("An owl")
+   st.image("https://static.streamlit.io/examples/owl.jpg")
 
 
 # def init_connection():
