df542fd1bb83a8b366d49448f211f96db6feb9ad
Ignition_Group <108255732+IGNITION-DISTRIBUTION-TEAM@users.noreply.github.com>
2023-03-16 22:42:21 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -31,7 +31,7 @@ rslt_df = DATAUPDATE.loc[DATAUPDATE['campaignname'] == option]
 col1, col2, col3 = st.columns(3)
 
 with col1:
-   st.line_chart(rslt_df,x="date",y="totalagents").jpg")
+   st.line_chart(rslt_df,x="date",y="totalagents")
 
 with col2:
    st.line_chart(rslt_df,x="date",y="totalsales")
