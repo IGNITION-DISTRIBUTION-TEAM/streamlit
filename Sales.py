575fe9e3f558cad634271ebaaa0a4c29e3a1838f
Ignition_Group <108255732+IGNITION-DISTRIBUTION-TEAM@users.noreply.github.com>
2023-04-26 13:29:59 +0200

    Update Sales.py

--- Sales.py
+++ Sales.py
@@ -20,4 +20,4 @@ DATAUPDATE = pd.read_sql(query, connection)
 
 with st.sidebar:
     option = st.selectbox('Please select a campaign',DATAUPDATE["campaignname"].unique())
-    option2 = st.selectbox('Please select a providername',DATAUPDATE["providername"].unique())
+    option2 = st.selectbox('Please select a providername',DATAUPDATE["providername"].unique(),, options=dummy_data[option])
