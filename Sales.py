041074a2ec8cb2354c07d5d7736989b97f96717d
Ignition_Group <108255732+IGNITION-DISTRIBUTION-TEAM@users.noreply.github.com>
2023-04-26 15:24:10 +0200

    Update Sales.py

--- Sales.py
+++ Sales.py
@@ -17,6 +17,11 @@ query = "select * from DATAWAREHOUSE.DISTRIBUTION_DATA_APPLICATION.VW_AD_SALES_U
 
 DATAUPDATE = pd.read_sql(query, connection)
 
+min_date = datetime.datetime(2020,1,1)
+max_date = datetime.date(2022,1,1)
+
+a_date = st.date_input("Pick a date", min_value=min_date, max_value=max_date)
+
 
 with st.sidebar:
     option = st.selectbox('Please select a campaign',DATAUPDATE["campaignname"].unique())
@@ -25,6 +30,16 @@ with st.sidebar:
 
     providertypes = DATAUPDATE["providertype"].loc[DATAUPDATE["campaignname"] == option]
     option3 = st.selectbox('Please select a providertype',providertypes.unique())
+    
+    start_date = st.date_input("Start Date", value=pd.to_datetime("2021-01-31", format="%Y-%m-%d"))
+    end_date = st.date_input("End Date", value=pd.to_datetime("today", format="%Y-%m-%d"))
+
+# convert the dates to string
+start = start_date.strftime("%Y-%m-%d")
+end = end_date.strftime("%Y-%m-%d")
+
+st.table(Series.loc[start:end]
+    
     
 df_filtered = DATAUPDATE[(DATAUPDATE['campaignname'] == option) & (DATAUPDATE['providername'] == option2) & (DATAUPDATE['providertype'] == option3)]
 
