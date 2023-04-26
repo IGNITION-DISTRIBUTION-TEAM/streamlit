d8f17cf72e72e11b910c960a4cfad2341f2df8e5
Ignition_Group <108255732+IGNITION-DISTRIBUTION-TEAM@users.noreply.github.com>
2023-04-26 13:42:07 +0200

    Update Sales.py

--- Sales.py
+++ Sales.py
@@ -20,6 +20,6 @@ DATAUPDATE = pd.read_sql(query, connection)
 
 with st.sidebar:
     option = st.selectbox('Please select a campaign',DATAUPDATE["campaignname"].unique())
-    providernames = df["providername"].loc[df["campaignname"] == option]
+    providernames = DATAUPDATE["providername"].loc[DATAUPDATE["campaignname"] == option]
     option2 = st.selectbox('Please select a providername',providernames)
 
