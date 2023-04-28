7f262350b4963259d08f78c1a2c4fa3d7f26c880
Ignition_Group <108255732+IGNITION-DISTRIBUTION-TEAM@users.noreply.github.com>
2023-04-28 13:49:40 +0200

    Update Sales.py

--- Sales.py
+++ Sales.py
@@ -35,9 +35,9 @@ with st.sidebar:
     start_date = st.date_input("Start Date", value=pd.to_datetime("2021-01-31", format="%Y-%m-%d"))
     end_date = st.date_input("End Date", value=pd.to_datetime("today", format="%Y-%m-%d"))
    
-mask = (DATAUPDATE['saledate'] >= start_date) & (DATAUPDATE['saledate'] <= end_date)
-# df_filtered = DATAUPDATE.loc[mask]
-# df_filtered = df_filtered[(DATAUPDATE['campaignname'] == option1) & (df_filtered['providername'] == option2) & (df_filtered['providertype'] == option3)]
+    mask = (DATAUPDATE['saledate'] >= start_date) & (DATAUPDATE['saledate'] <= end_date)
+    df_filtered = DATAUPDATE.loc[mask]
+    df_filtered = df_filtered[(DATAUPDATE['campaignname'] == option1) & (df_filtered['providername'] == option2) & (df_filtered['providertype'] == option3)]
 st.dataframe(df_filtered)    
 
 # metric = df_filtered['sales'].sum()
