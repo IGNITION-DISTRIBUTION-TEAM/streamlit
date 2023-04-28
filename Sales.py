df0ec527d27d6d30e477b263cfd495996a808be7
Ignition_Group <108255732+IGNITION-DISTRIBUTION-TEAM@users.noreply.github.com>
2023-04-28 15:22:10 +0200

    Update Sales.py

--- Sales.py
+++ Sales.py
@@ -57,7 +57,7 @@ st.altair_chart(c)
 
 
 d = alt.Chart(df_filtered).mark_bar().encode(
-    x='campaignname', 
+    x='salehour', 
     y='sum(sales)', 
     color = 'saledate:N',
     column='campaignname:N'
