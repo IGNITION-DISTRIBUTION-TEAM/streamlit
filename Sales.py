c8d350f371192e1ca7f83519d2f10e35e2e7ccf3
Ignition_Group <108255732+IGNITION-DISTRIBUTION-TEAM@users.noreply.github.com>
2023-05-02 12:53:51 +0200

    Update Sales.py

--- Sales.py
+++ Sales.py
@@ -12,7 +12,7 @@ url = URL(**st.secrets["snowflake"])
 st.set_page_config(layout="wide")
 st.title('Ignition Sales')
 
-@st.cache_data(ttl=60)
+@st.cache_data(ttl=3600)
 def load_data(url):
     engine = create_engine(url)
     connection = engine.connect()    
