a8a8589e6c3d84c35b7fa68c49f9bc473e8734a8
Ignition_Group <108255732+IGNITION-DISTRIBUTION-TEAM@users.noreply.github.com>
2023-05-03 08:16:04 +0200

    Update Sales.py

--- Sales.py
+++ Sales.py
@@ -78,7 +78,7 @@ st.altair_chart(d)
 
 df_average = df_filtered.groupby(['campaignname','salehour'])['sales'].mean()
 
-st.dataframe(df_average)
+st.dataframe(df_average['sales'])
 
 e = alt.Chart(df_average).mark_line().encode(
     x='salehour', 
