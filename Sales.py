8d1be5c9f91181b900d9117fe6a8523015ddb8d7
Ignition_Group <108255732+IGNITION-DISTRIBUTION-TEAM@users.noreply.github.com>
2023-05-03 12:48:06 +0200

    Update Sales.py

--- Sales.py
+++ Sales.py
@@ -23,17 +23,11 @@ def load_data(url):
     return DATAUPDATE
 
 snowflakedata = load_data(url)
-
-
-snowflakedata['saledate'] = pd.to_datetime(snowflakedata['saledate'], format='%Y-%m-%d')
+# snowflakedata['saledate'] = pd.to_datetime(snowflakedata['saledate'], format='%Y-%m-%d')
 
 current_date_mask = snowflakedata[(snowflakedata['saledate'] == current_date)]
-# df_singleday = snowflakedata.loc[current_date_mask]
-
 
 st.write(current_date)
-
-
 st.dataframe(current_date_mask)
 
 with st.sidebar:
