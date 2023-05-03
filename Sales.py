852a20eb7de4364266d3813e8d1f2bc8811f4bcb
Ignition_Group <108255732+IGNITION-DISTRIBUTION-TEAM@users.noreply.github.com>
2023-05-03 14:06:27 +0200

    Update Sales.py

--- Sales.py
+++ Sales.py
@@ -23,16 +23,7 @@ def load_data(url):
     return DATAUPDATE
 
 snowflakedata = load_data(url)
-# snowflakedata['saledate'] = pd.to_datetime(snowflakedata['saledate'], format='%Y-%m-%d')
-
-# current_date_mask = snowflakedata[(snowflakedata['saledate'] == current_date)]
 current_date_mask = snowflakedata.loc[(snowflakedata['saledate'] == pd.Timestamp(datetime.now()))]
-
-# current_date_mask = snowflakedata.loc[snowflakedata['saledate'] == pd.Timestamp(datetime.now()), 'saledate'].to_frame()
-
-st.write(pd.Timestamp(datetime.now()))
-st.dataframe(current_date_mask)
-st.dataframe(snowflakedata)
 
 with st.sidebar:
     snowflakedata = snowflakedata.sort_values(by=['campaignname'])
@@ -50,13 +41,18 @@ with st.sidebar:
     df_filtered = snowflakedata[snowflakedata['campaignname'].isin(option1) & (snowflakedata['providername'] == option2) & (snowflakedata['providertype'] == option3)]
     mask = (df_filtered['saledate'] >= start_date) & (df_filtered['saledate'] <= end_date)
     df_filtered = df_filtered.loc[mask]  
+    
+    current_date_mask_filtered = current_date_mask[current_date_mask['campaignname'].isin(option1) & (current_date_mask['providername'] == option2) & (current_date_mask['providertype'] == option3)]
+    mask2 = (current_date_mask_filtered['saledate'] >= start_date) & (current_date_mask_filtered['saledate'] <= end_date)
+    current_date_mask_filtered = df_filtered.loc[mask2]  
+    
 
 metric = df_filtered['sales'].sum()
 st.metric('Sales',metric)
 col1,col2 = st.columns(2)
 
 with col1:
-    c = alt.Chart(df_filtered).mark_line().encode(
+    c = alt.Chart(current_date_mask_filtered).mark_line().encode(
     x='salehour', 
     y='sum(sales)', 
     color = 'saledate:N'
