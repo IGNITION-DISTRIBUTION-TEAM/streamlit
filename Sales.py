506b8551b76f58d57e437b5d9652a8a90a6ade40
Ignition_Group <108255732+IGNITION-DISTRIBUTION-TEAM@users.noreply.github.com>
2023-05-03 13:57:04 +0200

    Update Sales.py

--- Sales.py
+++ Sales.py
@@ -26,7 +26,7 @@ snowflakedata = load_data(url)
 # snowflakedata['saledate'] = pd.to_datetime(snowflakedata['saledate'], format='%Y-%m-%d')
 
 # current_date_mask = snowflakedata[(snowflakedata['saledate'] == current_date)]
-current_date_mask = snowflakedata.loc[(snowflakedata['saledate'] < current_date)]
+current_date_mask = snowflakedata.loc[(snowflakedata['saledate'] == current_date)]
 
 st.write(current_date)
 st.dataframe(current_date_mask)
