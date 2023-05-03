b7b6810daba8189d49fc94a21b19da3c33ad965c
Ignition_Group <108255732+IGNITION-DISTRIBUTION-TEAM@users.noreply.github.com>
2023-05-03 12:28:49 +0200

    Update Sales.py

--- Sales.py
+++ Sales.py
@@ -27,7 +27,7 @@ snowflakedata = load_data(url)
 
 snowflakedata['saledate'] = pd.to_datetime(snowflakedata['saledate'], format='%Y-%m-%d')
 
-current_date_mask = snowflakedata['saledate'] == current_date
+current_date_mask = snowflakedata[(snowflakedata['saledate'] == current_date)]
 # df_singleday = snowflakedata.loc[current_date_mask]
 
 
