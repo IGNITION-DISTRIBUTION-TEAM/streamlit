d0e64ac8c6469a927c925535fd7eb006c6c2cc5e
Ignition_Group <108255732+IGNITION-DISTRIBUTION-TEAM@users.noreply.github.com>
2023-05-22 20:01:50 +0200

    Update Sales.py

--- Sales.py
+++ Sales.py
@@ -72,7 +72,7 @@ with col1:
     x='salehour', 
     y='sum(sales)', 
     color = 'saledate:N',
-    color=alt.Color('species', legend=None)
+    legend=None
     ).properties(
     width=800,
     height=500)
