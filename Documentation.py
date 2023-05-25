576038fe8b04474345232840ffd5deb3ffa3d027
Ignition_Group <108255732+IGNITION-DISTRIBUTION-TEAM@users.noreply.github.com>
2023-05-25 23:50:37 +0200

    Update Documentation.py

--- Documentation.py
+++ Documentation.py
@@ -1,1 +1,26 @@
+import streamlit as st
+from streamlit_extras.let_it_rain import rain
+import pandas as pd
+import numpy as np
+import snowflake.connector
+from snowflake.sqlalchemy import URL
+from sqlalchemy import create_engine
+from datetime import datetime
+import altair as alt
+
+current_date = datetime.today().strftime('%Y-%m-%d')+' 00:00:00'
+
+url = URL(**st.secrets["snowflake"])
+
+st.set_page_config(layout="wide",page_title="Documentation", page_icon="🌍")
+
+@st.cache_data(ttl=1500)
+def load_data(url):
+    engine = create_engine(url)
+    connection = engine.connect()    
+    query = "select * from DATAWAREHOUSE.DISTRIBUTION.TM_CAMPAIGN_DOCUMENTATION"    
+    DATAUPDATE = pd.read_sql(query, connection)
+    return DATAUPDATE
+
+snowflakedata = load_data(url)
 
