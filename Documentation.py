a44ed7db851e49d01b8bfd4d7ebe14808a28efcc
Ignition_Group <108255732+IGNITION-DISTRIBUTION-TEAM@users.noreply.github.com>
2023-05-26 00:07:49 +0200

    Update Documentation.py

--- Documentation.py
+++ Documentation.py
@@ -31,4 +31,4 @@ with st.sidebar:
 st.title(option1)    
 
 with st.expander("See explanation"):
-    st.write(\"\"\"The chart above shows some numbers I picked for you. I rolled actual dice for these, so they're *guaranteed* to be random.\"\"\")
+    st.write("The chart above shows some numbers I picked for you. I rolled actual dice for these, so they're *guaranteed* to be random.")
