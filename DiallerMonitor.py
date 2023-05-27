eeb2f3eac02a84dc5650aa3a2173558ddce8ca4f
Ignition_Group <108255732+IGNITION-DISTRIBUTION-TEAM@users.noreply.github.com>
2023-05-28 01:32:43 +0200

    Update DiallerMonitor.py

--- DiallerMonitor.py
+++ DiallerMonitor.py
@@ -1,1 +1,25 @@
+import streamlit as st
+from streamlit_extras.let_it_rain import rain
+import pandas as pd
+import numpy as np
+import snowflake.connector
+from snowflake.sqlalchemy import URL
+from sqlalchemy import create_engine
+from datetime import datetime
+import altair as alt
 
+current_date = datetime.today().strftime('%Y-%m-%d')+' 00:00:00'
+
+url = URL(**st.secrets["snowflake"])
+
+st.set_page_config(layout="wide",page_title="IG Sales", page_icon="🌍")
+
+# @st.cache_data(ttl=1500)
+# def load_data(url):
+#     engine = create_engine(url)
+#     connection = engine.connect()    
+#     query = "select * from DATAWAREHOUSE.DISTRIBUTION_DATA_APPLICATION.VW_AD_SALES_UPDATED"    
+#     DATAUPDATE = pd.read_sql(query, connection)
+#     return DATAUPDATE
+
+# snowflakedata = load_data(url)
